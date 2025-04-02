f9b812b9c2cafa836f84e766ade7b0368129e6bd
Nikomiko <ngool23@student.aau.dk>
2025-04-02 11:35:03 +0200

    Open files from different folders

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,5 @@
 open C64MC
+open CheckNames
 
 let () =
   if Array.length Sys.argv < 2 then
@@ -8,9 +9,12 @@ let () =
     let input_channel = open_in input_filename in
     try
       let lexbuf = Lexing.from_channel input_channel in
-      let _ast = Parser.prog Lexer.read lexbuf in
+      let ast = Parser.prog Lexer.read lexbuf in
       (* let output = Codegen.compile ast in *)
       (* Printf.printf "Output:\n%s\n" output; *)
-      close_in input_channel
+      
+      check_names ast ast.channel1;
+
+      close_in input_channel;
     with
     | e -> close_in input_channel; raise e
