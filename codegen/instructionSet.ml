1cb56e515952b88de15d36fc077582ffcd948ad3
Emandersen <minnyecomputer.com@gmail.com>
2025-04-07 11:03:46 +0200

    removed unnecessarylines

--- codegen/instructionSet.ml
+++ codegen/instructionSet.ml
@@ -1,69 +1,67 @@
-(* ---Instruction Set for the Commodore 64's 6502 Microprocessor---*)
-
-let test_func (x : int) = Printf.printf "%d \n" x;
-
+(* ---Instruction Set for the Commodore 64's 6502 Microprocessor--- *)
 
 type instruction = {
   name : string;
   mnemonic : string; 
-  (* mnemonics are the textual codes that correspond to binary CPU instructions *)
+  max_args : int;
+  min_args : int;
 }
 
 let instructions = [
-  { name = "Add with Carry"; mnemonic = "ADC" };
-  { name = "Logical AND"; mnemonic = "AND" };
-  { name = "Arithmetic Shift Left"; mnemonic = "ASL" };
-  { name = "Branch if Carry Clear"; mnemonic = "BCC" };
-  { name = "Branch if Carry Set"; mnemonic = "BCS" };
-  { name = "Branch if Equal"; mnemonic = "BEQ" };
-  { name = "Bit Test"; mnemonic = "BIT" };
-  { name = "Branch if Minus"; mnemonic = "BMI" };
-  { name = "Branch if Not Equal"; mnemonic = "BNE" };
-  { name = "Branch if Positive"; mnemonic = "BPL" };
-  { name = "Break"; mnemonic = "BRK" };
-  { name = "Branch if Overflow Clear"; mnemonic = "BVC" };
-  { name = "Branch if Overflow Set"; mnemonic = "BVS" };
-  { name = "Clear Carry Flag"; mnemonic = "CLC" };
-  { name = "Clear Decimal Mode"; mnemonic = "CLD" };
-  { name = "Clear Interrupt Disable"; mnemonic = "CLI" };
-  { name = "Clear Overflow Flag"; mnemonic = "CLV" };
-  { name = "Compare"; mnemonic = "CMP" };
-  { name = "Compare X Register"; mnemonic = "CPX" };
-  { name = "Compare Y Register"; mnemonic = "CPY" };
-  { name = "Decrement Memory"; mnemonic = "DEC" };
-  { name = "Decrement X Register"; mnemonic = "DEX" };
-  { name = "Decrement Y Register"; mnemonic = "DEY" };
-  { name = "Exclusive OR"; mnemonic = "EOR" };
-  { name = "Increment Memory"; mnemonic = "INC" };
-  { name = "Increment X Register"; mnemonic = "INX" };
-  { name = "Increment Y Register"; mnemonic = "INY" };
-  { name = "Jump"; mnemonic = "JMP" };
-  { name = "Jump to Subroutine"; mnemonic = "JSR" };
-  { name = "Load Accumulator"; mnemonic = "LDA" };
-  { name = "Load X Register"; mnemonic = "LDX" };
-  { name = "Load Y Register"; mnemonic = "LDY" };
-  { name = "Logical Shift Right"; mnemonic = "LSR" };
-  { name = "No Operation"; mnemonic = "NOP" };
-  { name = "Logical Inclusive OR"; mnemonic = "ORA" };
-  { name = "Push Accumulator"; mnemonic = "PHA" };
-  { name = "Push Processor Status"; mnemonic = "PHP" };
-  { name = "Pull Accumulator"; mnemonic = "PLA" };
-  { name = "Pull Processor Status"; mnemonic = "PLP" };
-  { name = "Rotate Left"; mnemonic = "ROL" };
-  { name = "Rotate Right"; mnemonic = "ROR" };
-  { name = "Return from Interrupt"; mnemonic = "RTI" };
-  { name = "Return from Subroutine"; mnemonic = "RTS" };
-  { name = "Subtract with Carry"; mnemonic = "SBC" };
-  { name = "Set Carry Flag"; mnemonic = "SEC" };
-  { name = "Set Decimal Flag"; mnemonic = "SED" };
-  { name = "Set Interrupt Disable"; mnemonic = "SEI" };
-  { name = "Store Accumulator"; mnemonic = "STA" };
-  { name = "Store X Register"; mnemonic = "STX" };
-  { name = "Store Y Register"; mnemonic = "STY" };
-  { name = "Transfer Accumulator to X"; mnemonic = "TAX" };
-  { name = "Transfer Accumulator to Y"; mnemonic = "TAY" };
-  { name = "Transfer Stack Pointer to X"; mnemonic = "TSX" };
-  { name = "Transfer X to Accumulator"; mnemonic = "TXA" };
-  { name = "Transfer X to Stack Pointer"; mnemonic = "TXS" };
-  { name = "Transfer Y to Accumulator"; mnemonic = "TYA" };
+  { name = "Add with Carry"; mnemonic = "ADC"; max_args = 2; min_args = 1 };
+  { name = "Logical AND"; mnemonic = "AND"; max_args = 2; min_args = 1 };
+  { name = "Arithmetic Shift Left"; mnemonic = "ASL"; max_args = 1; min_args = 1 };
+  { name = "Branch if Carry Clear"; mnemonic = "BCC"; max_args = 1; min_args = 1 };
+  { name = "Branch if Carry Set"; mnemonic = "BCS"; max_args = 1; min_args = 1 };
+  { name = "Branch if Equal"; mnemonic = "BEQ"; max_args = 1; min_args = 1 };
+  { name = "Bit Test"; mnemonic = "BIT"; max_args = 2; min_args = 1 };
+  { name = "Branch if Minus"; mnemonic = "BMI"; max_args = 1; min_args = 1 };
+  { name = "Branch if Not Equal"; mnemonic = "BNE"; max_args = 1; min_args = 1 };
+  { name = "Branch if Positive"; mnemonic = "BPL"; max_args = 1; min_args = 1 };
+  { name = "Break"; mnemonic = "BRK"; max_args = 0; min_args = 0 };
+  { name = "Branch if Overflow Clear"; mnemonic = "BVC"; max_args = 1; min_args = 1 };
+  { name = "Branch if Overflow Set"; mnemonic = "BVS"; max_args = 1; min_args = 1 };
+  { name = "Clear Carry Flag"; mnemonic = "CLC"; max_args = 0; min_args = 0 };
+  { name = "Clear Decimal Mode"; mnemonic = "CLD"; max_args = 0; min_args = 0 };
+  { name = "Clear Interrupt Disable"; mnemonic = "CLI"; max_args = 0; min_args = 0 };
+  { name = "Clear Overflow Flag"; mnemonic = "CLV"; max_args = 0; min_args = 0 };
+  { name = "Compare"; mnemonic = "CMP"; max_args = 2; min_args = 1 };
+  { name = "Compare X Register"; mnemonic = "CPX"; max_args = 2; min_args = 1 };
+  { name = "Compare Y Register"; mnemonic = "CPY"; max_args = 2; min_args = 1 };
+  { name = "Decrement Memory"; mnemonic = "DEC"; max_args = 1; min_args = 1 };
+  { name = "Decrement X Register"; mnemonic = "DEX"; max_args = 0; min_args = 0 };
+  { name = "Decrement Y Register"; mnemonic = "DEY"; max_args = 0; min_args = 0 };
+  { name = "Exclusive OR"; mnemonic = "EOR"; max_args = 2; min_args = 1 };
+  { name = "Increment Memory"; mnemonic = "INC"; max_args = 1; min_args = 1 };
+  { name = "Increment X Register"; mnemonic = "INX"; max_args = 0; min_args = 0 };
+  { name = "Increment Y Register"; mnemonic = "INY"; max_args = 0; min_args = 0 };
+  { name = "Jump"; mnemonic = "JMP"; max_args = 1; min_args = 1 };
+  { name = "Jump to Subroutine"; mnemonic = "JSR"; max_args = 1; min_args = 1 };
+  { name = "Load Accumulator"; mnemonic = "LDA"; max_args = 2; min_args = 1 };
+  { name = "Load X Register"; mnemonic = "LDX"; max_args = 2; min_args = 1 };
+  { name = "Load Y Register"; mnemonic = "LDY"; max_args = 2; min_args = 1 };
+  { name = "Logical Shift Right"; mnemonic = "LSR"; max_args = 1; min_args = 1 };
+  { name = "No Operation"; mnemonic = "NOP"; max_args = 0; min_args = 0 };
+  { name = "Logical Inclusive OR"; mnemonic = "ORA"; max_args = 2; min_args = 1 };
+  { name = "Push Accumulator"; mnemonic = "PHA"; max_args = 0; min_args = 0 };
+  { name = "Push Processor Status"; mnemonic = "PHP"; max_args = 0; min_args = 0 };
+  { name = "Pull Accumulator"; mnemonic = "PLA"; max_args = 0; min_args = 0 };
+  { name = "Pull Processor Status"; mnemonic = "PLP"; max_args = 0; min_args = 0 };
+  { name = "Rotate Left"; mnemonic = "ROL"; max_args = 1; min_args = 1 };
+  { name = "Rotate Right"; mnemonic = "ROR"; max_args = 1; min_args = 1 };
+  { name = "Return from Interrupt"; mnemonic = "RTI"; max_args = 0; min_args = 0 };
+  { name = "Return from Subroutine"; mnemonic = "RTS"; max_args = 0; min_args = 0 };
+  { name = "Subtract with Carry"; mnemonic = "SBC"; max_args = 2; min_args = 1 };
+  { name = "Set Carry Flag"; mnemonic = "SEC"; max_args = 0; min_args = 0 };
+  { name = "Set Decimal Flag"; mnemonic = "SED"; max_args = 0; min_args = 0 };
+  { name = "Set Interrupt Disable"; mnemonic = "SEI"; max_args = 0; min_args = 0 };
+  { name = "Store Accumulator"; mnemonic = "STA"; max_args = 2; min_args = 1 };
+  { name = "Store X Register"; mnemonic = "STX"; max_args = 2; min_args = 1 };
+  { name = "Store Y Register"; mnemonic = "STY"; max_args = 2; min_args = 1 };
+  { name = "Transfer Accumulator to X"; mnemonic = "TAX"; max_args = 0; min_args = 0 };
+  { name = "Transfer Accumulator to Y"; mnemonic = "TAY"; max_args = 0; min_args = 0 };
+  { name = "Transfer Stack Pointer to X"; mnemonic = "TSX"; max_args = 0; min_args = 0 };
+  { name = "Transfer X to Accumulator"; mnemonic = "TXA"; max_args = 0; min_args = 0 };
+  { name = "Transfer X to Stack Pointer"; mnemonic = "TXS"; max_args = 0; min_args = 0 };
+  { name = "Transfer Y to Accumulator"; mnemonic = "TYA"; max_args = 0; min_args = 0 };
 ]
