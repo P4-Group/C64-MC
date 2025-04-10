27b05296fa08ee8254bb29a0a7cf1224db1df04b
ceci000 <SK22VJ@student.aau.dk>
2025-04-10 10:28:17 +0200

    print seq id symbol table

--- lib/symbol_table.ml
+++ lib/symbol_table.ml
@@ -3,7 +3,6 @@ type memory_address = int
 
 type symbol_info = (* a record data structure used for grouping related information *)
   | SequenceSymbol of { (* the key is the sequence id, the value is sequence and mem address *)
-    (* id : string;  *)
     seq : Ast.seq; (* list of notes *)
     mutable mem_address : memory_address option; (* the memory address where the sequence is stored *)
     (* the memory address is type mutable, meaning the value can change. we do this because we
@@ -30,8 +29,9 @@ let add_sequence id seq =
     failwith "Sequences id's cannot be duplicated. Each sequence must have a unique id.";
 
   let symbol = SequenceSymbol {seq = seq; mem_address = None} in 
-  Hashtbl.add symbol_table id symbol
-  
+  Hashtbl.add symbol_table id symbol;
+  Printf.printf "Symbol table id: %s \n" id
+
 
 (* Checks if the sequence id exists. If not, an error will be thrown. *)
 let check_sequence id =
