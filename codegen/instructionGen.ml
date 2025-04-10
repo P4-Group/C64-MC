4ac35a254dddd3362166b94ed92d73eafd182a49
Emandersen <minnyecomputer.com@gmail.com>
2025-04-10 14:54:00 +0200

    added new error handling in instructionsgen.ml

--- codegen/instructionGen.ml
+++ codegen/instructionGen.ml
@@ -1,4 +1,12 @@
 open InstructionSet
+(*open Exceptions*) (*Uncomment when connected to rest of the program*)
+
+
+(*Just for testing purposes*)
+exception InstructionNotFoundError of string
+exception InsufficientInstructionArguments of string * int * int
+exception TooManyInstructionArguments of string * int * int
+
 
 (* Write a line to a file without overwriting existing lines *)
 let write_line_tf (line : string) =
@@ -11,13 +19,14 @@ let write_line_tf (line : string) =
 let construct_instruction (mnemonic : string) (args : string list) =
   let instruction = List.find_opt (fun instr -> instr.mnemonic = mnemonic) instructions in
   match instruction with
-  | None -> failwith (Printf.sprintf "Error: Instruction '%s' not found" mnemonic)
+  | None -> raise (InstructionNotFoundError (Printf.sprintf "Instruction '%s' not found" mnemonic))
   | Some instr ->
       let arg_count = List.length args in
+      Printf.eprintf "Argument count: %d\n" arg_count;
       if arg_count < instr.min_args then
-        failwith (Printf.sprintf "Error: Not enough arguments provided for '%s'. Expected at least %d, but got %d" mnemonic instr.min_args arg_count)
+        raise (InsufficientInstructionArguments (mnemonic, instr.min_args, arg_count))
       else if arg_count > instr.max_args then
-        failwith (Printf.sprintf "Error: Too many arguments provided for '%s'. Expected at most %d, but got %d" mnemonic instr.max_args arg_count)
+        raise (TooManyInstructionArguments (mnemonic, instr.max_args, arg_count))
       else
         let constructed_instruction = Printf.sprintf "%s %s" instr.mnemonic (String.concat ", " args) in
         constructed_instruction
@@ -35,12 +44,12 @@ let construct_labelled_instructions (label : string) (instruction_table : (strin
 let () =
   let args = ["10"; "20"] in
   let constructed_instruction = construct_instruction "ADC" args in
-  write_line_tf constructed_instruction;
+  write_line_tf constructed_instruction; 
 
   let label = "MyLabel" in
   let instruct_hashtbl = Hashtbl.create 10 in
-  Hashtbl.add instruct_hashtbl "LDA" ["$00"];
+  Hashtbl.add instruct_hashtbl "LDA" ["$00"; "#$FF"];
   Hashtbl.add instruct_hashtbl "STA" ["$01"];
-  Hashtbl.add instruct_hashtbl "JMP" ["$02"];
+  Hashtbl.add instruct_hashtbl "JMP" ["$02"; "$02"; "$02"; "$02"];
   construct_labelled_instructions label instruct_hashtbl;
     
