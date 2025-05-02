b9d0952c7cfe7a8434d765f9223aca3d9248f4f3
Emandersen <minnyecomputer.com@gmail.com>
2025-05-02 09:39:36 +0200

    hotfix for windows, as well as cleanup of RO

--- lib/runtime_options.ml
+++ lib/runtime_options.ml
@@ -1,3 +1,5 @@
+open Exceptions
+
 type t = {
   mutable tgt_ast : bool;
   mutable src_ast : bool;
@@ -32,3 +34,51 @@ let conditional_option debug_options run =
     run ()
   else
     ()
+
+
+(* This checks the parameters of the input file and handling errors. *)
+let check_file_params () =
+  let params = Array.to_list (Array.sub Sys.argv 1 (Array.length Sys.argv - 1)) in
+  let src_file_ref = ref None in
+
+  let rec parse_args args =
+    match args with
+    | [] -> () (* Base case: no more arguments *)
+
+    (* Boolean flags *)
+    | "-tgt-ast" :: rest ->
+        set_tgt_ast true;
+        parse_args rest
+    | "-src-ast" :: rest ->
+        set_src_ast true;
+        parse_args rest
+    | "-debug" :: rest ->
+        set_debug true;
+        parse_args rest
+    | "-sym-tab" :: rest ->
+        set_sym_tab true;
+        parse_args rest
+    | "-dasm" :: rest -> 
+        set_dasm true;
+        parse_args rest
+
+    (* Arguments with values *)
+    | "-s" :: file :: rest ->
+        if !src_file_ref <> None then
+          raise (InsufficientArguments "Option '-s' (source file) specified multiple times.");
+        src_file_ref := Some file;
+        parse_args rest
+    | "-s" :: [] ->
+        raise (InsufficientArguments "Option '-s' requires a file path argument.")
+
+    (* Unknown argument *)
+    | unknown :: _ ->
+        raise (InsufficientArguments ("Invalid argument or unknown option: " ^ unknown ^
+          "\nUsage: <program> -s <source_file> [-dasm] [-tgt-ast] [-src-ast] [-sym-tab] [-debug]"))
+  in
+  parse_args params;
+
+  (* Check if the mandatory source file argument was provided *)
+  match !src_file_ref with
+  | Some file -> file (* Return the source file path *)
+  | None -> raise (InsufficientArguments "Missing mandatory source file argument '-s <file>'.")
