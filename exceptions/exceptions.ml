09a670733f6db5c706dfb453b45af4086cb62c98
ceci000 <SK22VJ@student.aau.dk>
2025-05-02 15:32:06 +0200

    cleaned up and fixed exceptions

--- exceptions/exceptions.ml
+++ exceptions/exceptions.ml
@@ -18,29 +18,15 @@ exception InstructionNotFoundError of string
 exception InsufficientInstructionArgumentsError of string * int * int
 exception TooManyInstructionArgumentsError of string * int * int
 
-(*-------------------------*)
-(*Parser Exceptions*)
-(*-------------------------*)
 
-exception InvalidOctaveError of string
-exception InvalidDurationError of string
-exception InvalidWaveformError of string
 
-(*-------------------------*)
-(*Symbol Table Exceptions*)
-(*-------------------------*)
-exception DuplicateSequenceError of string
-exception MissingSequenceError of string
-exception MissingMemoryAddressError of string
 exception InvalidArgumentError of string
-
 
 (*-------------------------*)
 (*Utils Exceptions*)
 (*-------------------------*)
-exception InvalidToneError of string
 
 (*-------------------------*)
 (*AST Translate Exceptions*)
 (*-------------------------*)
-exception InvalidTimeSignatureError of string
+exception InvalidTimeSignatureError of string 
